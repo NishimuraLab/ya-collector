5547ee2df5265a686a67ced69f031f942542aaf3
Yota Nagahata <swimy_inf@yahoo.co.jp>
2016-12-01 19:21:50 +0900

    fix script

--- scripts/item_texts_arrange_script.rb
+++ scripts/item_texts_arrange_script.rb
@@ -8,15 +8,22 @@ require 'erb'
 config = YAML.load(ERB.new(File.read('database/config.yml')).result)
 ActiveRecord::Base.establish_connection(config['db']['development'])
 
-def can_include?(line)
-  !line.include?("記号") || !line.include?("地域") || !line.include?("助動詞") || !line.include?("助詞")
+def not_include?(line)
+  line.include?("記号") || line.include?("地域") || line.include?("助動詞") || line.include?("助詞")
 end
 
 Item.where(non_tagged_description: nil).find_each(batch_size: 25) do |item|
   description = item.description
-  non_tagged = description.gsub(/<.+?>/, '')
-  nm = Natto::MeCab.new(output_format_type: :wakati)
-  wakati = nm.parse(non_tagged)
+  non_tagged = description.gsub(/<.+?>/, '').gsub(/\&nbsp/, '')
+
+  nm = Natto::MeCab.new
+  mecabed = nm.parse(non_tagged)
+  mecabed.split('\n').each do |line|
+    next if not_include?(line)
+
+  end
+  nm_wakati = Natto::MeCab.new(output_format_type: :wakati)
+  wakati = nm_wakati.parse(non_tagged)
   begin
     ActiveRecord::Base.transaction do
       item.non_tagged_description = wakati
