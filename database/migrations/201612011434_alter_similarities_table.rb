fa080e6f2bc5266567b2a35d9497c365d74107bf
Yota Nagahata <swimy_inf@yahoo.co.jp>
2016-12-01 19:21:30 +0900

    fix migration

--- database/migrations/201612011434_alter_similarities_table.rb
+++ database/migrations/201612011434_alter_similarities_table.rb
@@ -1,8 +1,8 @@
 class AlterSimilaritiesTable < ActiveRecord::Migration
   def change
-    rename_column :similarities, :auction_id, :label
-    rename_column :similarities, :pair_auction_id, :pair_label
+    rename_column :similarities, :auction_id, :label_id
+    rename_column :similarities, :pair_auction_id, :pair_label_id
 
-    add_index :similarities, [:label, :pair_label]
+    add_index :similarities, [:label_id, :pair_label_id]
   end
 end
